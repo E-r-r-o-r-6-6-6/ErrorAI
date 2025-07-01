c5ff09380861673858ec2ba895207449cf2e575b
E_R_R_O_R_6.6.6 <sibunbehera7656@gmail.com>
2025-07-01 11:04:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # app.py
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)  # Allows frontend from GitHub to access this API
@@ -12,4 +13,5 @@ def respond():
     return jsonify({'reply': f"You said: {message}"})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host='0.0.0.0', port=port)
