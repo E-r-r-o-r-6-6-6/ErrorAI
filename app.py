55343145cc6a815f24b0d606fdbb20fb5cb8b42b
E_R_R_O_R_6.6.6 <sibunbehera7656@gmail.com>
2025-07-01 15:07:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,15 +2,22 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import os
+from g4f.client import Client
 
 app = Flask(__name__)
-CORS(app)  # Allows frontend from GitHub to access this API
+CORS(app)
 
 @app.route('/api', methods=['POST'])
 def respond():
     data = request.get_json()
-    message = data.get('message', '')
-    return jsonify({'reply': f"You said: {message} , but i said: nothing 🤪"})
+    usermessage = data.get('message', '')
+    client = Client()
+    response = client.chat.completions.create(
+	    model = "gpt-4o-mini"
+	    messages = [{"role":"user","content":f"{usermessage}"}]
+	)
+    reply = response.choices[0].message.content 
+    return jsonify(reply)
 
 if __name__ == '__main__':
 	port = int(os.environ.get("PORT", 5000))
